ed17c9fe8b653e2fc6a7fe953e8f5cb6eed66a27
Bruno FL <follet-bruno@hotmail.fr>
2019-05-02 15:14:00 +0200

    Test e2e with testcafé

--- e2e/SelectUnique.js
+++ e2e/SelectUnique.js
@@ -8,12 +8,19 @@ test('Selection', async t => {
   let item1 = await Selector('#HidenburgExplosion-hb_2-hb_2_1-item-1')
   let item2 = await Selector('#HidenburgExplosion-hb_2-hb_2_1-item-2')
   await t
-    .setNativeDialogHandler(() => true)
-    .expect(list.visible).eql(false, 'List not visible')
-    .click(input)
+    .expect(input.exists).ok()
+    .expect(item1.exists).ok()
+    .expect(item2.exists).ok()
+
+    .expect(list.visible).eql(false, 'Liste not visible')
     .expect(input.value).eql('Choisir ...', 'Etat de base de l\'input')
+
+    .click(input)
+    .expect(list.visible).eql(true, 'Liste visible')
+
     .click(item1)
     .expect(input.value).eql(await item1.innerText, 'Input prend la valeur de l\'item1')
+
     .click(item2)
     .expect(input.value).eql(await item2.innerText, 'Input prend la valeur de l\'item2')
 });
