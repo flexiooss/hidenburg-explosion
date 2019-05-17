ddcf3b2a363a762b96aebf596a49953667a7885b
Bruno FL <follet-bruno@hotmail.fr>
2019-05-17 14:26:42 +0200

    test focus

---
+++ e2e/Select.js
@@ -0,0 +1,17 @@
+import Page from "./Page";
+
+fixture('Select').page('https://ui.flexio.io:8080/');
+
+const page = new Page()
+
+test('Je peux ouvrir et fermer les select avec la touche TAB', async t => {
+  await t
+    .expect(page.listU.visible).eql(false, 'Liste non visible')
+    .expect(page.listM.visible).eql(false, 'Liste non visible')
+    .pressKey('tab')
+    .expect(page.listU.visible).eql(true, 'Liste non visible')
+    .expect(page.listM.visible).eql(false, 'Liste non visible')
+    .pressKey('tab')
+    .expect(page.listU.visible).eql(false, 'Liste non visible')
+    .expect(page.listM.visible).eql(true, 'Liste non visible')
+})
--- e2e/SelectUnique.js
+++ e2e/SelectUnique.js
@@ -19,17 +19,17 @@ test('La liste est visible quand je clique sur le champ, sinon disparait', async
 
 test('Je peux selectionner un seul item dans une liste unique', async t => {
   await t
-    .expect(page.inputU.value).eql('Choisir ...', 'Etat de base de l\'page.inputU')
+    .expect(page.inputU.value).eql('Choisir ...', 'Etat de base de l\'input')
     .click(page.inputU)
     .click(page.itemU1)
-    .expect(page.inputU.value).eql(await page.itemU1.innerText, 'Input prend la valeur de l\'page.itemU1')
+    .expect(page.inputU.value).eql(await page.itemU1.innerText, 'Input prend la valeur de itemU1')
     .expect(page.itemU1.getAttribute('selected')).eql('true')
     .expect(page.itemU2.getAttribute('selected')).eql('false')
 
 
     .click(page.inputU)
     .click(page.itemU2)
-    .expect(page.inputU.value).eql(await page.itemU2.innerText, 'Input prend la valeur de l\'page.itemU2')
+    .expect(page.inputU.value).eql(await page.itemU2.innerText, 'Input prend la valeur de itemU2')
     .expect(page.itemU1.getAttribute('selected')).eql('false')
     .expect(page.itemU2.getAttribute('selected')).eql('true')
 });
@@ -39,7 +39,7 @@ test('Je peux faire un shift clic, mais simule un clic normal', async t => {
     .expect(page.inputU.value).eql('Choisir ...', 'Etat de base de l\'input')
     .click(page.inputU)
     .click(page.itemU2, {modifiers: {shift: true}})
-    .expect(page.inputU.value).eql(await page.itemU2.innerText, 'Input prend la valeur de l\'page.itemU2')
+    .expect(page.inputU.value).eql(await page.itemU2.innerText, 'Input prend la valeur de itemU2')
     .expect(page.itemU1.getAttribute('selected')).eql('false')
     .expect(page.itemU2.getAttribute('selected')).eql('true')
     .expect(page.listU.visible).eql(false, 'Liste non visible')
